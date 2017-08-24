e87f85753dd3ed4ad3a07cf0be36a03c3c831def
Jess Poemape <jesspoemape@gmail.com>
2017-08-24 13:58:06 -0600

    server set up. some dummy data.

---
+++ src/server.js/dummyDB.js
@@ -0,0 +1,116 @@
+module.exports = {
+    "Users": [
+        {
+            "id": 1,
+            "username": "jaelarue",
+            "email": "jaelarue@jlr.com"
+        },
+        {
+            "id": 2,
+            "username": "juanjjpoem",
+            "email": "jjpd@jjpd.com"
+        },
+        {
+            "id": 3,
+            "username": "potatoluvr",
+            "email": "potts@email.com"
+        },
+        {
+            "id": 4,
+            "username": "coolguy38",
+            "email": "coolguy@38.com"
+        }
+    ],
+    "Habits": [
+        {
+            "id": 1,
+            "habit_name": "Read for ten minutes",
+            "date_created": "2017-07-24T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-08-02T19:49:26.874Z"
+        },
+        {
+            "id": 2,
+            "habit_name": "Run 1 mile",
+            "date_created": "2017-07-25T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-07-27T19:49:26.874Z"
+        },
+        {
+            "id": 3,
+            "habit_name": "Walk the dog",
+            "date_created": "2017-07-24T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-08-12T19:49:26.874Z"
+        },
+        {
+            "id": 4,
+            "habit_name": "Eat a healthy breakfast",
+            "date_created": "2017-07-04T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-08-02T19:49:26.874Z"
+        },
+        {
+            "id": 5,
+            "habit_name": "Don't smoke",
+            "date_created": "2017-07-24T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-07-24T19:49:26.874Z"
+        },
+        {
+            "id": 6,
+            "habit_name": "Make the bed",
+            "date_created": "2017-08-20T19:49:26.874Z",
+            "user_id": "1",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 7,
+            "habit_name": "Feed the cat",
+            "date_created": "2017-08-19T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 8,
+            "habit_name": "Feed the dog",
+            "date_created": "2017-08-24T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 9,
+            "habit_name": "Floss",
+            "date_created": "2017-08-24T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 10,
+            "habit_name": "Walk 10,000 steps",
+            "date_created": "2017-08-24T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 11,
+            "habit_name": "Don't chew on fingernails",
+            "date_created": "2017-08-24T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        },
+        {
+            "id": 12,
+            "habit_name": "Don't drink soda",
+            "date_created": "2017-08-24T19:49:26.874Z",
+            "user_id": "2",
+            "current_streak_start_date": "2017-08-24T19:49:26.874Z"
+        }
+    ],
+    "CheckIns": [
+        {
+            "habit_id": 1,
+            "checkin_at": ""
+        },
+    ]
+}
--- src/server.js/server.js
+++ src/server.js/server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+//=========ENDPOINTS===========
+
+
+
+
 app.listen(3000, () => {
     console.log('Listening on port 3000')
 });
